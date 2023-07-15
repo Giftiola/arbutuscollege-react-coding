f5856dffd1eacaaab8e04471f85d4e9c3518cc6e
Giftiola <52719101+Giftiola@users.noreply.github.com>
2023-07-14 17:11:43 -0700

    CourseList.js

--- React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import CourseListRow from './CourseListRow';
-import CourseShape from './CourseShape';
-import PropTypes from 'prop-types';
-import './CourseList.css';
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import PropTypes from "prop-types";
+import CourseShape from "./CourseShape";
+import "./CourseList.css";
 
-const CourseList = ({ listCourses }) => {
+function CourseList({ listCourses }) {
   return (
     <table id="CourseList">
-      {listCourses.length > 0 ? (
-        <>
-          <thead>
-            <CourseListRow
-              textFirstCell={'Available courses'}
-              isHeader={true}
-            />
-            <CourseListRow
-              textFirstCell={'Course name'}
-              textSecondCell={'Credit'}
-              isHeader={true}
-            />
-          </thead>
-          <tbody>
-            {listCourses.map((course) => (
-              <CourseListRow
-                key={course.id}
-                textFirstCell={course.name}
-                textSecondCell={course.credit}
-              />
-            ))}
-          </tbody>
-        </>
-      ) : (
-        <tbody>
-          <CourseListRow textFirstCell={'No course available yet'} />
-        </tbody>
-      )}
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow
+          textFirstCell="Course name"
+          textSecondCell="Credit"
+          isHeader={true}
+        />
+      </thead>
+      <tbody>
+        {listCourses.length === 0 && (
+          <CourseListRow
+            textFirstCell="No course available yet"
+            isHeader={false}
+          />
+        )}
 
-      <tbody></tbody>
+        {listCourses.map((course) => (
+          <CourseListRow
+            key={course.id}
+            textFirstCell={course.name}
+            textSecondCell={course.credit}
+            isHeader={false}
+          />
+        ))}
+      </tbody>
     </table>
   );
-};
+}
 
 CourseList.defaultProps = {
   listCourses: [],
